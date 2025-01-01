115ae83d5fb7877ab7208ae385ccecc86a0cf056
Prashantk45 <Prashantsk.272@gmail.com>
2025-01-02 03:35:53 +0530

    n
    n

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Welcome to METTICA</h1>
+      <p className="mt-4 text-xl">Your Aerospace Hub</p>
+    </div>
+  );
+}
